aae639b6694121f5e0148312f9db2ddd7d1d98b2
Manish Kumar <memanish2525@gmail.com>
2025-10-26 14:51:11 +0530

    Day-1

---
+++ script.js
@@ -0,0 +1,67 @@
+// Today learn Topic📢
+// introduction to js
+// why it is impotant
+// what can it do for you
+// script tag use karke attach krna hai js ko.
+// js ko browser mein run krna hai.
+// var let const 
+// console... prompt alert
+// string kya hota hai
+// slice template strings splite replace indcludes
+
+
+
+// 1.    introduction to js👈
+//  :--- js ek scripting language hai. computer ko binary smjh aati hai 0101010.
+//  :--- js ko 1995 mein brendan eich ne banaya tha.
+// or hmara language high level language hai. jo human ko smjh aati hai.
+// or computer ko low level language smjh aati hai.
+// to hmara or computer ke bich ek translator hota hai 
+// jo hmare language ko binery me smjhta hai or us transltor ka nam 
+// compiler ya interpreter hota hai.
+// manish => compiler => binary
+
+
+
+
+// 2.   why it is impotant👈
+// kyu ki wesite pe animation  -> interaction -> laghbhag har prakaar ka koe engagement create krne ke liye js use hoti hai
+
+
+
+//  console... prompt alert👈
+// console.log("hello manish");
+// console.warn("this is a warning");
+// console.error("this is an error");
+// alert("this is an alert");
+// var name = prompt("enter your name");
+// console.log(name);
+
+
+// string kya hota hai👈
+// string ek data type hai. jisme hum text store krte hai.
+// var name = "manish";
+// var friend = 'harry';
+// var fruit = `banana`;
+// console.log(name);
+// console.log(friend);
+// console.log(fruit);
+// console.log(typeof name);
+// slice template strings splite replace indcludes👈
+
+// var name = "manish kumar singh";
+// var newName = name.slice(0,6);
+// console.log(newName);
+
+// var friend = "harry potter";
+// var newFriend = friend.split(`a`);
+// console.log(newFriend);
+
+
+// var fruit = "banana";
+// var newFruit = fruit.replace(`ban`,`123`);
+// console.log(newFruit);
+
+// var city = "delhi";
+// var check = city.includes(`l`);
+// console.log(check);
