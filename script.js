c2f84b29198b1155d96fa972b56af5d13c88f962
Manish kumar <169014705+manishkumar2543@users.noreply.github.com>
2025-10-29 21:36:20 +0530

    Day-3

--- script.js
+++ script.js
@@ -1,102 +1,75 @@
-// var let const
-// consle... prompt alert
-// string kya hoti hai
-// slice template -string (template literals ``) splite replace include
-// statements expression
+// Data Types📢
+
+// add comments
+// float
+// number
+// String
+// null
+// boolean
+// array
+// object
+// symbol - will learn when will see object in detail
+// undefined
+// NaN
+// Infinity
+// relative and primitive datatype in js.
 
 
-// var let const 👈
-// var -> function scope
-// let, const -> block scope
-// const -> constant value (cannot be reassigned)
-// let -> can be reassigned
+// 📢 add comments :- double // kre ke lgate ise error kuch nhi ata hai.
+// Ex :-- hdhjjdjjdjdjjdjdjdjjdjdj  (but nor error).
 
-// var name = "John";
-// name = "Doe";
-// console.log(name); // Doe
+// float : -- 12.3 , 23.3%
+// number :-- 123- integer
+// string :- manih
+// null :-  jab apke apke pas koe value nhi hotaa dene ke liye to null use hota hai.
+// Ex:- let a=null
+// console.log(null); 
+// output :- null
 
-// let age = 25;
-// age = 26;
-// console.log(age); // 26
-// const pi = 3.14;
-// // pi = 3.1415; // Error: Assignment to constant variable.
-// console.log(pi); // 3.14
+// boolean :- false,true
 
-// {
-//     var city = "New York";
-//     // var pure function scoped hai to yeh bahar bhi accessible hai
-// }
-// console.log(city); // New York
+// array : --  ek se jada value add krne ke liye hai.
+// Ex: --   [1,2,3,4];
+//    ["harsh", "shyam", "ravi"];
+//     [1,2,"harsh",true,{},[],class Animal {}]
+// Object :--  ek se jada properties ek bande ki hold krne ke liye hai.
+// object ek bande ki kai sari cheejo ke liye hota hai.
+            // Ex:-- just like information
+                //    {
+                //     name:"Manish",
+                //     age:23,
+                //     email: "harsh@email.com"
+                //    }
+// symbol :-  will learn when we will see objects in detail.
 
-// {
-//     let country = "USA";
-//     console.log(country); // USA
-// }
-// console.log(country); // Error: country is not defined
+// undefind :- jab koi variable banaya but usey koe value nhi di.
+// Ex:--
+        // var a;
+        // console.log(a)
 
-
-// console.log("heyheyhey");
-// console.info("heyeyeyebase");
-// console.warn("bewarebeware");
-// console.table({name: "John", age: 30});
-
-// Kuch kuch browsers bas console.info ko thode alag tareeke se show krte
-// unke aage ek i sign aajata hai.
-
-
-// let users=prompt("name ")
-// console.log(users)
-
-
-// alert
-// alert("18+ adult")
-
-
-// slice👈
-// let nam="manish thora acha se"
-// console.log(nam.slice(6,12))
-
-
-// `` template strings👈
-
-// string 
-// let a="manish 2+2"
+// Nan :- koe number ke sathh aisa operation kiya jo ho nhi skta hai.
+// Ex:-- jo possible n ho.
+// let a= "mans"*2;
 // console.log(a)
+// output : Nan
 
-// let b="mansih"+2+2;
-// console.log(b)
-// mansih22
+// infinity :-  infinity
+//  let a= 1/0;
+//  console.log(a)
 
-// template strings👈
-// let c=`manish ${2+2}`;
-// console.log(c);
-// manish 4
+// relative and primitive datatype in js.
+// Ex:-- inki value ko direct copy nhi kr skte hai
+var a= [1,2,3,4];
+// or barket value ko iss trh se copy nhi kr skte hai
+// b me a ka copy nhi gya hai b me a refernce gya hai
+var b=a;
+b.pop()
+console.log(b)
+console.log(a)
+// b me change krne pr a me bhi change ho rha hai to hm inki real value ko change nhi kr skte hai.
 
-// split 👈👈
+// reference vali value :--  array object function.
+// primitives - number booleans strings null undefined Nan infinity symbol BigInt
 
-// let name="manish kumar";
-// console.log(name.split(` `))
-
-// let name="mansih bhai kaise ho";
-// console.log(name.split('h'))
-// [ 'mansi', ' b', 'ai kaise ', 'o' ]
-
-// Replace 👈 -- ise sirf pahila word replace hoga
-// //  
-// let name="manish bhai kaise ho";
-// console.log(name.replace('h','g'))
-
-// replaceAll --> ise pura word me ka replace hoga✔👈
-// let name="manish molhtra sing";
-// console.log(name.replaceAll('m','w'))
-
-// indclue --> ye check krta hai ki exit krta hai y nhi👈
-
-// let name="manish";
-// console.log(name.includes('m'))
-
-
-// statements ---> full line ka code jise last mein(;)
-
-// expressions
-// 12+33=ye value produce krega to isko expression bolenge
+// Primitives :- jinki value direct copy kr skte hai
+// reference - jinki value direct copy nhi kr skte hai
